ac1b3605302a3a6b1b400ab03ac59e67fcd9093d
Abhishek Bhayye <abhishek.bhayye@iloy-group.com>
2021-04-13 15:39:57 +0100

    Basic application without services and forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipeBookApplication';
+
+  loadedFeature = 'recipes';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
